8e49460366a4fa24a02cb11c1936d758f9a12b68
Sebastián Barraud <sebastian.barraud@gmail.com>
2018-06-28 14:32:57 -0300

    Crecion de container para conduct y fix del navbar

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React from 'react'
 
-import { BrowserRouter, Route } from "react-router-dom";
-import Home from "../containers/Home";
-import Navbar from "../containers/Navbar";
-import About from "../containers/About";
-import Preregister from "../containers/Preregister";
-import Program from "../containers/Program";
-import SubmitedForm from "../containers/SubmitedForm";
+import { BrowserRouter, Route } from 'react-router-dom'
+import Home from '../containers/Home'
+import Navbar from '../containers/Navbar'
+import About from '../containers/About'
+import Preregister from '../containers/Preregister'
+import Program from '../containers/Program'
+import SubmitedForm from '../containers/SubmitedForm'
+import Conduct from '../containers/Conduct'
 
 export default () => (
   <BrowserRouter>
@@ -17,6 +18,7 @@ export default () => (
       <Route path="/iodc_2018/preregister" exact component={Preregister} />
       <Route path="/iodc_2018/submited-form" exact component={SubmitedForm} />
       <Route path="/iodc_2018/program" exact component={Program} />
+      <Route path="/iodc_2018/conduct" exact component={Conduct} />
     </div>
   </BrowserRouter>
-);
+)
