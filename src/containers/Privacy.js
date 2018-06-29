4415325fdfcbd6ab8a130fae8697fa26c3c69f00
Sebastián Barraud <sebastian.barraud@gmail.com>
2018-06-29 15:55:27 -0300

    Cambios en Privacy

--- src/containers/Privacy.js
+++ src/containers/Privacy.js
@@ -31,69 +31,90 @@ class Privacy extends Component {
                 information, how we use it, the limited conditions under which
                 we may disclose it to others and how we keep it secure.
               </p>
-              <p className="internal-text">
-                <span className="bold-title">Data Use</span> IODC does not sell,
-                rent or lease your personal data under any circumstances. IODC
-                uses the information you provide to send you communications,
-                such as Conference program updates; ticketing information; and
-                other relevant Conference information.
+
+              <p className="internal-sub-title internal-sub-title-low mtop40">
+                Data Use
               </p>
               <p className="internal-text">
-                <span className="bold-title">Type of Data We Collect</span>
+                IODC does not sell, rent or lease your personal data under any
+                circumstances. IODC uses the information you provide to send you
+                communications, such as Conference program updates; ticketing
+                information; and other relevant Conference information.
+              </p>
+              <p className="internal-sub-title internal-sub-title-low mtop40">
+                Type of Data We Collect
               </p>
 
-              <p className="internal-text-list-program">
+              <p className="internal-text">
                 <ul>
-                  <li>
+                  <li className="mtop20">
                     <span class="bullet">●</span>
-                    Google Analytics - When someone visits opendatacon.org, we
-                    use a third party service, Google Analytics, to collect
-                    standard internet log information and details of visitor
-                    behaviour patterns. We do this to understand who we reached
-                    and how we can reach more people to our conference. This
-                    information is only processed in a way which does not
-                    identify anyone. We do not make, and do not allow Google to
-                    make, any attempt to find out the identities of those
-                    visiting our website.
+                    <span className="bold-text">Google Analytics</span>{' '}
+                    <p>
+                      When someone visits opendatacon.org, we use a third party
+                      service, Google Analytics, to collect standard internet
+                      log information and details of visitor behaviour patterns.
+                      We do this to understand who we reached and how we can
+                      reach more people to our conference. This information is
+                      only processed in a way which does not identify anyone. We
+                      do not make, and do not allow Google to make, any attempt
+                      to find out the identities of those visiting our website.
+                    </p>
                   </li>
-                  <li>
+                  <li className="mtop20">
                     <span class="bullet">●</span>
-                    Registration Forms - We collect your contact details in our
-                    registration form so that we can:
+                    <span className="bold-text">Registration Forms</span>{' '}
+                    <p>
+                      We collect your contact details in our registration form
+                      so that we can:
+                    </p>
                     <ul>
                       <li>
-                        1. Verify your identification when you visit the
-                        conference.
+                        <span className="bold-text">1.</span> Verify your
+                        identification when you visit the conference.
                       </li>
                       <li>
-                        2. Contact you so you can keep up-to-date with the
-                        conference and have the best attendance experience.{' '}
+                        <span className="bold-text">2. </span>Contact you so you
+                        can keep up-to-date with the conference and have the
+                        best attendance experience.{' '}
                       </li>
-                      <li>3. Measure the diversity of the event. </li>
+                      <li>
+                        <span className="bold-text">3. </span>Measure the
+                        diversity of the event.{' '}
+                      </li>
                     </ul>
                   </li>
-                  <li>
+                  <li className="mtop20">
                     <span class="bullet">●</span>
-                    Newsletter - By registering to the conference, you give us
-                    consent to include you in our mailing list. Your data will
-                    only be shared with MailChimp to send you correspondance
-                    about the conference. You can unsubscribe from MailChimp at
-                    anytime. For more information, please see MailChimp’s
-                    privacy notice.
+                    <span className="bold-text">Newsletter</span>{' '}
+                    <p>
+                      By registering to the conference, you give us consent to
+                      include you in our mailing list. Your data will only be
+                      shared with MailChimp to send you correspondance about the
+                      conference. You can unsubscribe from MailChimp at anytime.
+                      For more information, please see{' '}
+                      <a
+                        className="links-ext"
+                        target="_blank"
+                        href="https://mailchimp.com/legal/privacy/"
+                      >
+                        MailChimp’s privacy notice.
+                      </a>
+                    </p>
                   </li>
                 </ul>
               </p>
 
               <p className="internal-text">
-                <span className="bold-title">
+                <p className="internal-sub-title internal-sub-title-low mtop40">
                   Access to your Personal Information
-                </span>{' '}
+                </p>
                 You are entitled to view, amend, or delete the personal
                 information of yours that we maintain. Email your request to
                 contact@opendatacon.org
               </p>
 
-              <p className="internal-text">
+              <p className="internal-text mbottom75">
                 <span className="bold-title">
                   Changes to this Privacy Notice
                 </span>{' '}
