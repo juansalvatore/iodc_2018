c8b72b96ac19021958d88e3faba62c783271432a
Sebastián Barraud <sebastian.barraud@gmail.com>
2018-07-02 14:56:34 -0300

    Ajustes de sumbitted form

--- src/containers/SubmitedForm.js
+++ src/containers/SubmitedForm.js
@@ -4,15 +4,24 @@ import { IntlProvider } from 'react-intl'
 import messages from './messages'
 import { setLocale } from '../actions/locale'
 
+import { Header, Footer } from './'
+
 class SubmitedForm extends Component {
   render() {
     const { lang } = this.props
     return (
       <IntlProvider locale={lang} messages={messages[lang]}>
-        <div>
-          <br />
-          <br />
-          Submited Form
+        <div className="container-fluid">
+          <Header>Register</Header>
+
+          <div className="row">
+            <div className="col-md-10 col-md-offset-1">
+              <h3 className="internal-sub-title submitted-form-success">
+                You have been registered successfully.
+              </h3>
+            </div>
+          </div>
+          <Footer />
         </div>
       </IntlProvider>
     )
