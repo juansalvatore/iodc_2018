2fb3136635c5af33726301136e38906314fb7b50
Sebastián Barraud <sebastian.barraud@gmail.com>
2018-07-02 18:49:23 -0300

    Ajustes del menu de Dropdown

--- src/img/index.js
+++ src/img/index.js
@@ -14,6 +14,7 @@ import Obelisco from './obelisco.svg'
 import Visas from './visas.svg'
 import Invitacion from './invitacion.svg'
 import AboutImg from './about-section.jpg'
+import Medium from './medium.svg'
 export {
   Logo,
   Facebook,
@@ -31,4 +32,5 @@ export {
   Visas,
   Invitacion,
   AboutImg,
+  Medium,
 }
