abe75ec3c6f8773a3823d532756885521bc2d1c2
Juan Salvatore <juansalvatore@live.com.ar>
2018-07-04 20:51:24 -0300

    Arregla press y h3 para centrar titulos

--- src/containers/Press.js
+++ src/containers/Press.js
@@ -11,14 +11,19 @@ class Press extends Component {
     const { lang } = this.props
     return (
       <IntlProvider locale={lang} messages={messages[lang]}>
-        <div>
+        <div className="press_container">
           <div className="container-fluid">
             <Header>
               <FormattedHTMLMessage id="press.header" defaultMessage="Info" />
             </Header>
             <div className="row press_container">
               <div className="col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2 centerelement">
-                <img src={IdCard} alt="IdCard" className="img-idcard" />
+                <img
+                  id="id_card"
+                  src={IdCard}
+                  alt="IdCard"
+                  className="img-idcard"
+                />
                 <p classNameName="internal-text text-center">
                   <FormattedHTMLMessage id="press.text" defaultMessage="Info" />
                 </p>
